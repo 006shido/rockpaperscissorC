fd712e48e377757540790500afd42c751eb3c153
Anoop <006shido@gmail.com>
2026-06-06 23:40:54 +0530

    Refactor Rock Paper Scissors game with scoring modes

--- main.c
+++ main.c
@@ -3,42 +3,112 @@
 #include <time.h>
 
 int main() {
-    int user, computer;
+
+    int mode;
+    char playAgain;
 
     srand(time(NULL));
 
-    printf("=== Rock Paper Scissors ===\n");
-    printf("1. Rock\n");
-    printf("2. Paper\n");
-    printf("3. Scissors\n");
-
-    printf("Enter your choice (1-3): ");
-    scanf("%d", &user);
-
-    computer = rand() % 3 + 1;
-
-    printf("\n");
-
-    // Display of choices
     char *choices[] = {"", "Rock", "Paper", "Scissors"};
 
-    printf("You chose: %s\n", choices[user]);
-    printf("Computer chose: %s\n", choices[computer]);
+    printf("================================\n");
+    printf("    ROCK PAPER SCISSORS GAME\n");
+    printf("================================\n");
 
-    // Determination of winner
-    if (user == computer) {
-        printf("Result: Draw!\n");
-    }
-    else if (
-        (user == 1 && computer == 3) ||
-        (user == 2 && computer == 1) ||
-        (user == 3 && computer == 2)
-    ) {
-        printf("Result: You Win!\n");
-    }
-    else {
-        printf("Result: Computer Wins!\n");
-    }
+    do {
+
+        int playerScore = 0;
+        int computerScore = 0;
+
+        printf("\nChoose Game Mode:\n");
+        printf("1. Best of 3\n");
+        printf("2. Best of 5\n");
+        printf("Enter choice: ");
+
+        while (scanf("%d", &mode) != 1 || (mode != 1 && mode != 2)) {
+            while (getchar() != '\n');
+            printf("Invalid choice. Enter 1 or 2: ");
+        }
+
+        int roundsNeeded = (mode == 1) ? 2 : 3;
+        int maxRounds = (mode == 1) ? 3 : 5;
+        int round = 1;
+
+        while (playerScore < roundsNeeded &&
+               computerScore < roundsNeeded) {
+
+            int playerChoice;
+            int computerChoice = rand() % 3 + 1;
+
+            printf("\n================================\n");
+            printf("          ROUND %d/%d\n", round, maxRounds);
+            printf("================================\n");
+
+            printf("1. Rock\n");
+            printf("2. Paper\n");
+            printf("3. Scissors\n");
+            printf("Enter your choice: ");
+
+            while (scanf("%d", &playerChoice) != 1 ||
+                   playerChoice < 1 ||
+                   playerChoice > 3) {
+
+                while (getchar() != '\n');
+                printf("Invalid input! Enter 1, 2 or 3: ");
+            }
+
+            printf("\nYou Chose      : %s\n", choices[playerChoice]);
+            printf("Computer Chose : %s\n", choices[computerChoice]);
+
+            if (playerChoice == computerChoice) {
+
+                printf("\nResult : Draw!\n");
+
+            } else if (
+                (playerChoice == 1 && computerChoice == 3) ||
+                (playerChoice == 2 && computerChoice == 1) ||
+                (playerChoice == 3 && computerChoice == 2)
+            ) {
+
+                playerScore++;
+                printf("\nResult : You Win This Round!\n");
+
+            } else {
+
+                computerScore++;
+                printf("\nResult : Computer Wins This Round!\n");
+            }
+
+            printf("\nCurrent Score\n");
+            printf("----------------------------\n");
+            printf("You      : %d\n", playerScore);
+            printf("Computer : %d\n", computerScore);
+
+            round++;
+        }
+
+        printf("\n================================\n");
+
+        if (playerScore > computerScore) {
+
+            printf("CONGRATULATIONS! YOU WON THE MATCH!\n");
+
+        } else {
+
+            printf("COMPUTER WON THE MATCH!\n");
+        }
+
+        printf("Final Score: You %d - %d Computer\n",
+               playerScore, computerScore);
+
+        printf("================================\n");
+
+        printf("\nPlay Again? (y/n): ");
+        scanf(" %c", &playAgain);
+
+    } while (playAgain == 'y' || playAgain == 'Y');
+
+    printf("\nThanks for playing!\n");
 
     return 0;
 }
